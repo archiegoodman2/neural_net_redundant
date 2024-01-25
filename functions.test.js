c18b727ac273893b95e9e0d2b6764aae71a24d1c
Mike Menzter fan <92301571+archiegoodman2@users.noreply.github.com>
2024-01-25 22:11:21 +0000

    Update functions.test.js
    added tests for error messages

--- functions.test.js
+++ functions.test.js
@@ -22,7 +22,14 @@ test('getting dotproduct of two vectors', () => {
   expect(dotProduct(vector1, vector2)).toBe(20);
 });
 
-//this one is going to cause problems - need to check the logic in the actual functions file, I don't think it's correct
+test('correct error message when 2 vectors are wrong size', () => {
+  const vector1 = [1, 2, 3] 
+  const vector2 = [2, 3, 4, 5]
+
+  expect(dotProduct(vector1, vector2)).toBe(20);
+});
+
+
 test('getting dotproduct of a vector and a 2D matrix', () => {
   const vector = [1, 2, 3] 
   const matrix = [[2, 3, 4],
